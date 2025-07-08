557388b78cf2aa32a2886806d0f84eb79ac99c07
kolibri753 <kolibri753@gmail.com>
2025-07-08 15:33:07 +0300

    perf(main): use import.meta.env.DEV for React Query devtools

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -13,9 +13,7 @@ createRoot(document.getElementById("root")!).render(
       <ThemeProvider>
         <App />
       </ThemeProvider>
-      {process.env.NODE_ENV === "development" && (
-        <ReactQueryDevtools initialIsOpen={false} />
-      )}
+      {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
     </QueryClientProvider>
   </StrictMode>
 );
