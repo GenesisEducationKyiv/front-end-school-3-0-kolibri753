7c4a3c63001e9333ed98f2fb68d9726b62b81cfa
kolibri753 <kolibri753@gmail.com>
2025-07-16 21:09:45 +0300

    test(vitest): remove storybook integration

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -1,16 +1,7 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-// import path from "node:path";
-// import { fileURLToPath } from "node:url";
-// import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
 
-// const dirname =
-//   typeof __dirname !== "undefined"
-//     ? __dirname
-//     : path.dirname(fileURLToPath(import.meta.url));
-
-// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   test: {
@@ -21,32 +12,6 @@ export default defineConfig({
       "src/**/*.msw.int.test.{ts,tsx}",
     ],
     testTimeout: 30000,
-    // projects: [
-    //   {
-    //     extends: true,
-    //     plugins: [
-    //       // The plugin will run tests for the stories defined in your Storybook config
-    //       // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-    //       storybookTest({
-    //         configDir: path.join(dirname, ".storybook"),
-    //       }),
-    //     ],
-    //     test: {
-    //       name: "storybook",
-    //       browser: {
-    //         enabled: true,
-    //         headless: true,
-    //         provider: "playwright",
-    //         instances: [
-    //           {
-    //             browser: "chromium",
-    //           },
-    //         ],
-    //       },
-    //       setupFiles: [".storybook/vitest.setup.ts"],
-    //     },
-    //   },
-    // ],
     coverage: {
       provider: "v8",
       reportsDirectory: "./coverage",
@@ -59,6 +24,9 @@ export default defineConfig({
         "src/**/*.int.test.{ts,tsx}",
         "src/**/*.msw.int.test.{ts,tsx}",
         "src/**/*.ct.{ts,tsx}",
+
+        "src/**/*.stories.{ts,tsx}",
+        "src/**/*.story.{ts,tsx}",
 
         "**/*.d.ts",
         "**/vite-env.d.ts",
