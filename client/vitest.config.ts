66c72f429095de906f02f3a3b4b7318031627816
kolibri753 <kolibri753@gmail.com>
2025-07-16 20:19:41 +0300

    test(vitest): comment out not used imports

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
+// import path from "node:path";
+// import { fileURLToPath } from "node:url";
 // import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
 
 // const dirname =
